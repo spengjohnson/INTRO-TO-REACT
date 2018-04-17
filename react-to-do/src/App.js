a237331f5eaec1ede7d1facbbec518eae4169117
spjohnson <spgant@gmail.com>
2018-04-17 08:53:46 -0600

    adding state and props again with changes to ToDo.js

--- react-to-do/src/App.js
+++ react-to-do/src/App.js
@@ -6,21 +6,21 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-    todos: [
-      { description: 'Walk the dog', isCompleted: true },
-      { description: 'Throw the dishes away', isCompleted: false },
-      { description: 'Buy new dishes', isCompleted: false }
-    ]
-  };
-}
+      todos: [
+        { description: 'Walk the cat', isCompleted: true },
+        { description: 'Throw the dishes away', isCompleted: false },
+        { description: 'Buy new dishes', isCompleted: false }
+      ]
+    };
+  }
 
   render() {
     return (
       <div className="App">
        <ul>
-       { this.state.todos.map( (todo, index) =>
-         <ToDo key={ index } />
-        )}
+         { this.state.todos.map( (todo, index) =>
+           <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted }/>
+         )}
        </ul>
       </div>
     );
